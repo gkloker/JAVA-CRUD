90bc07b3bb872a8cf0651efa9c453e1e8b635b16
Gabriel Kloker <gabrielkloker@gmail.com>
2021-08-04 12:35:59 -0300

    -

--- src/main/java/net/java/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/net/java/springbootbackend/SpringbootBackendApplication.java
@@ -19,13 +19,9 @@ public class SpringbootBackendApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-          "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-          "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-          "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 
